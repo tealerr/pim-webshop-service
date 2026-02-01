088fa642531c2523bd5dc4369271b16c721f2b31
tealerr <theetitle@gmail.com>
2026-02-02 05:06:24 +0700

    refactor: restructture config

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+// Main entrypoint for the application
+// Load environment variables first
+import "dotenv/config"
+// Importing the server module triggers the server startup.
+import "./routes/server"
+
+export default undefined
--- src/service/db.ts
+++ src/service/db.ts
@@ -1,23 +1,12 @@
 // db.ts
 import mongoose from "mongoose"
-import { any } from "webidl-conversions"
+import { dbConfig } from "../config/database"
 
 // Connect to MongoDB
 const connectDB = async () => {
     try {
-        // username = doadmin
-        // password = 0Jp1Y8j25bKZ649r hide
-        // host = mongodb+srv://db-mongodb-sgp1-43271-7f9131f1.mongo.ondigitalocean.com
-        // database = admin
-        console.log("Connecting...")
-        // "mongodb+srv://doadmin:0Jp1Y8j25bKZ649r@db-mongodb-sgp1-43271-7f9131f1.mongo.ondigitalocean.com/admin?retryWrites=true"
-        await mongoose.connect(
-            "mongodb+srv://doadmin:0Jp1Y8j25bKZ649r@db-mongodb-sgp1-43271-7f9131f1.mongo.ondigitalocean.com/admin",
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            } as any
-        )
+        console.log("Connecting to MongoDB...")
+        await mongoose.connect(dbConfig.uri, dbConfig.options as any)
         console.log("Connected to MongoDB")
     } catch (error) {
         console.error("Error connecting to MongoDB:", error)
